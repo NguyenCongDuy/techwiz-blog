98d625ee94766f0f7173dc9c7590f0029affca84
NguyenCongDuy <146914945+NguyenCongDuy@users.noreply.github.com>
2025-06-13 13:55:15 +0700

    Fix duplicate /build in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+// xoa build roi nhung ma khong hieu tai sao van hien thi loi   
 export default defineConfig({
     plugins: [
         laravel({
